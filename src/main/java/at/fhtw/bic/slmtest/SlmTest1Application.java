cebad17736a3d758d76555e80994481486430018
anita <ic22b055@fh-technikum.at>
2023-01-26 18:15:30 +0100

    Initial commit

---
+++ src/main/java/at/fhtw/bic/slmtest/SlmTest1Application.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic.slmtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmTest1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmTest1Application.class, args);
+    }
+
+}
---
+++ src/test/java/at/fhtw/bic/slmtest/SlmTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.bic.slmtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmTest1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
